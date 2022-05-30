50046058c28f7018a7f407baec9dc985cbe3e58a
Venkatesh Krishnamurthy <71044775+venkkris@users.noreply.github.com>
2022-05-30 17:53:16 -0400

    Updated job.sh and utilities/mkjob.sh to specify mpi in srun command after Arjuna major update in May 2022

--- job.sh
+++ job.sh
@@ -1,21 +1,21 @@
-#!/bin/bash                                                                                      
-#                                                                                                
-#SBATCH -J job_name
-#SBATCH -n 54
+#!/bin/bash
+#
+#SBATCH -J b442_Li2
+#SBATCH -n 60
 #SBATCH -N 1
 #SBATCH --time=7-00:00
-#SBATCH -A venkvis
-#SBATCH -p cpu
+#SBATCH -A venkvis_gpu
+#SBATCH -p gpu
 #SBATCH --mem=0
 #SBATCH -o job.sh.o%j
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=venkatek@andrew.cmu.edu
 
 
-echo "Job started on `hostname` at `date`"
+echo Job started on `hostname` at `date`
 
 spack load py-gpaw
-srun -n 54 gpaw python relax_magnetic.py
+srun --mpi=pmix -n 60 gpaw python relax.py
 
-echo " "
-echo "Job Ended at `date`"
+echo  
+echo Job Ended at `date`
